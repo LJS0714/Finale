9aca3e82a9fe22fb27591bc8f357e454769dee2a
LJS0714 <109053223+LJS0714@users.noreply.github.com>
2022-12-20 16:03:26 +0900

    Update sketch.js
    makeing frame of the tree

--- sketch.js
+++ sketch.js
@@ -1,20 +1,16 @@
 let system, system2, system3;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1000, 500);
   system = new ParticleSystem(createVector(width / 2, 50));
   system2 = new ParticleSystem(createVector(width/3, 100));
   system3 = new ParticleSystem(createVector(width*2/3, 100));
-  
+
 }
 
 function draw() {
   
-  
   background(255);
-  
-  fill(128,0,0);
-  rect(width/2,40,10,height);
   
   system.addParticle();
   system.run();
@@ -25,5 +21,39 @@ function draw() {
   system3.addParticle();
   system3.run();
   
+  newTree();
   
+}
+
+function newTree() {
+
+  stroke(0);
+  push();
+  translate(width/2, height);
+  branch(120);
+  pop();
+  
+}
+
+
+
+function branch(h) {
+  var sw = map(h, 2, 120, 1, 5);
+  strokeWeight(sw);
+  line(0, 0, 0, -h);
+  translate(0, -h);
+  
+
+  h *= 0.66;
+
+  if (h > 2) {
+    var n = Math.floor(random(1, 4));
+    for (var i = 0; i < n; i++) {
+      var theta = random(-PI/3, PI/3);
+      push();   
+      rotate(theta);    
+      branch(h);         
+      pop();       
+    }
+  }
 }
